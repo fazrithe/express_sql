a80b1feda4668fe5f81079d9fa818c341e8efb98
fazri-airmasgroup <59469844+fazri-airmasgroup@users.noreply.github.com>
2021-03-17 13:51:01 +0700

    add form html
    add form html

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser
 const express = require('express'),
+path = require('path')
 app = express(),
 mysql = require('mysql'),
 cors = require('cors'),
@@ -18,11 +19,19 @@ var server = {
     port: 4040
 };
 
+
 const usersRouter = require('./users');
+
 // use the modules
 app.use(cors())
-app.use(bodyParser.json());
+app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.use('/users', usersRouter);
+
+app.get('/', function(req, res) {
+    res.sendFile(path.resolve(__dirname,'views') + '/input.html');
+});
 
 app.listen(server.port, () => console.log(`Server started, listening on server : ${server.port}`))
